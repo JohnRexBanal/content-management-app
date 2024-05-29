4a0576909a4e58ef48b03496e44237befaa78220
rinoaxyrl <almodovarrinoaxyrel73@gmail.com>
2024-05-29 23:51:07 +0800

    modified the LoginPage file

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -2,15 +2,17 @@
     <div class="container mt-5 col-3">
       <div class="row">
         <h4>Enter your email and password</h4>
+        <form @submit.prevent="loginUser" class="col-12">
           <div class="form-group mt-3">
-            <input type="email" class="form-control" id="email"  placeholder="Enter email" >
+            <input type="email" class="form-control" id="email" v-model="email" placeholder="Enter email" required @input="clearErrors">
           </div>
           <div class="form-group mt-3">
-            <input type="password" class="form-control" id="password" placeholder="Password" >
+            <input type="password" class="form-control" id="password" v-model="password" placeholder="Password" required @input="clearErrors">
   
           </div>
           <button type="submit" class="btn btn-primary mt-3 w-100">Login</button>
-      </div>
+        </form>
+        </div>
       <div class="row">
         <div class="col-12">
             <p class="mt-3 text-left">Don't have an account? <router-link to="/register">Register here!</router-link></p>
