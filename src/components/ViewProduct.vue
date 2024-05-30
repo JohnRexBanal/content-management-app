6137caf246ee563d2dc59c1bd3e575e88a752c59
aragonescyrelle78 <aragonescyrelle78@gmail.com>
2024-05-31 00:09:20 +0800

    Product view component template

---
+++ src/components/ViewProduct.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container mt-5">
+      <router-link to="/products" class="btn btn-primary mb-3">Back</router-link>
+      <div class="card" v-if="product">
+        <div class="card-header bg-dark text-white">
+          <h3>{{ product.name }}</h3>
+        </div>
+        <div class="card-body">
+          <h5>Description: {{ product.description }}</h5>
+          <p>Price: ${{ product.price }}</p>
+          <p>Stocks: {{ product.stock }}</p>
+        </div>
+      </div>
+      <div v-else>
+        <p>Loading...</p>
+      </div>
+    </div>
+  </template>
